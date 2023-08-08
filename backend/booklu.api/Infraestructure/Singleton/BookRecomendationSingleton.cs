2c610bd3ee8fe690fa539a730c8e94d9966b149f
Byron Duarte <byronduarte95@gmail.com>
2023-08-08 12:51:04 -0500

    singleton pattern: book recomendation class with unique global access

---
+++ backend/booklu.api/Infraestructure/Singleton/BookRecomendationSingleton.cs
@@ -0,0 +1,43 @@
+﻿namespace booklu.api.Infraestructure.Singleton
+{
+    public class BookRecomendationSingleton
+    {
+        private static readonly object _lock = new object();
+        private static BookRecomendationSingleton _instance = null;
+        private List<string> recomendations = new List<string>();
+
+        //private constructor
+        private BookRecomendationSingleton()
+        {
+        }
+
+
+        //global method access point
+        public static BookRecomendationSingleton Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    lock (_lock) // Asegura que solo un hilo pueda acceder a este bloque a la vez
+                    {
+                        _instance = new BookRecomendationSingleton();
+                    }
+                }
+                    
+                return _instance;
+            }    
+        }
+
+        public void AddRecomendation(string libro)
+        {
+            recomendations.Add(libro);
+        }
+
+        public List<string> GetRecomendations()
+        {
+            return recomendations;
+        }
+
+    }
+}
