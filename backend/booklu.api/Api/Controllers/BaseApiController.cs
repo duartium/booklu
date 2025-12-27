4984ecc82741ea697a9d0efab1c726d9141bb213
duartium <byronduarte95@gmail.com>
2025-12-26 22:25:03 -0500

    feat: replacement automapper for mapperly

--- backend/booklu.api/Api/Controllers/BaseApiController.cs
+++ backend/booklu.api/Api/Controllers/BaseApiController.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace booklu.api.Api.Controllers;
@@ -10,11 +9,9 @@ public class BaseApiController<T> : ControllerBase where T : BaseApiController<T
 {
     private IMediator _mediator;
     private ILogger<T> _logger;
-    protected IMapper _mapper;
     public IConfiguration _configuration;
 
     protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     protected ILogger<T> Logger => _logger ??= HttpContext.RequestServices.GetService<ILogger<T>>();
-    protected IMapper Mapper => _mapper ??= HttpContext.RequestServices.GetService<IMapper>();
     protected IConfiguration Configuration => _configuration ??= HttpContext.RequestServices.GetService<IConfiguration>();
 }
